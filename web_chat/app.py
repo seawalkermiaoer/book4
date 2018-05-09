05a0e6fb3f3929378ca10891ca3d7bb42b9bd514
seawalkermiaoer <seawalkerxh@gmail.com>
2018-05-09 21:33:18 +0800

    ico
    ico

--- web_chat/app.py
+++ web_chat/app.py
@@ -1,21 +1,13 @@
 # -*- coding:utf-8 -*-
 # __author__ = 'xianghai'
-import json
-import urllib.parse
-
-import sys
-
-import os
-from pprint import pprint
 
 from gevent.wsgi import WSGIServer
 
-import requests
-from flask import Flask, request
+from flask import Flask
 from flask import render_template
-from flask_restful import reqparse, Api, Resource
 
 app = Flask(__name__)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
