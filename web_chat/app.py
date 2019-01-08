6c96b578aabd81f8e9a8d4b132702c2b97dc28fe
seawalkermiaoer <seawalkerxh@gmail.com>
2019-01-08 11:02:46 +0800

    调整结构

--- web_chat/app.py
+++ web_chat/app.py
@@ -1,10 +1,15 @@
 # -*- coding:utf-8 -*-
 # __author__ = 'xianghai'
 
-from gevent.pywsgi  import WSGIServer
+from gevent.pywsgi import WSGIServer
 
 from flask import Flask
 from flask import render_template
+from flask_restful import Api
+from gevent.pywsgi import WSGIServer
+from flask_cors import CORS
+
+from web_chat.qa import SmartQA
 
 app = Flask(__name__)
 
@@ -15,4 +20,8 @@ def index():
 
 
 if __name__ == '__main__':
+	CORS(app, supports_credentials=True)
+	# 同义词更新
+	api = Api(app)
+	api.add_resource(SmartQA, "/qa")
 	WSGIServer(("0.0.0.0", 8888), app).serve_forever()
--- web_chat/get_tuling.py
+++ web_chat/get_tuling.py
@@ -5,8 +5,14 @@ import requests
 
 def get_chatting(content):
 	rsp = requests.get("http://www.tuling123.com/openapi/api?key=49de46c409c047d19b2ed2285e8775a6&info=" + content)
-	rsp.json()
-	
+	answer = rsp.json()
+	print(answer)
+	if "code" in answer and answer["code"] == 100000:
+		return answer["text"]
+	else:
+		return "需要再学习一下呢"
+
+
 # {"code":100000,"text":"你陪我玩我就好啦"}
-if __name__ == '__main__':
-	get_chatting("你好")
+# if __name__ == '__main__':
+# 	get_chatting("你好")
