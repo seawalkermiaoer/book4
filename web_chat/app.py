658b4fad9502939e936240f9be72732f0ec5637e
xianghai <xianghai@xiaoduotech.com>
2019-08-06 17:09:47 +0800

    add log sdk

--- web_chat/app.py
+++ web_chat/app.py
@@ -1,27 +1,25 @@
 # -*- coding:utf-8 -*-
 # __author__ = 'xianghai'
 
-from gevent.pywsgi import WSGIServer
-
 from flask import Flask
 from flask import render_template
 from flask_restful import Api
 from gevent.pywsgi import WSGIServer
 from flask_cors import CORS
 
-from web_chat.qa import SmartQA
+from qa import SmartQA
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-	return render_template('index.html')
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
-	CORS(app, supports_credentials=True)
-	# 同义词更新
-	api = Api(app)
-	api.add_resource(SmartQA, "/qa")
-	WSGIServer(("0.0.0.0", 8888), app).serve_forever()
+    CORS(app, supports_credentials=True)
+    # 同义词更新
+    api = Api(app)
+    api.add_resource(SmartQA, "/qa")
+    WSGIServer(("0.0.0.0", 8888), app).serve_forever()
--- web_chat/qa.py
+++ web_chat/qa.py
@@ -1,19 +1,18 @@
 from flask import request
 from flask_restful import Resource
-from web_chat.get_tuling import get_chatting
+from get_tuling import get_chatting
 
 
 class SmartQA(Resource):
-	
-	def __del__(self):
-		pass
-	
-	def post(self):
-		try:
-			text = request.form["text"]
-			print(text)
-			return {"succ": True, "text": get_chatting(text)}, 200
-		except Exception as e:
-			print(e)
-			return {"text": "error while qa", "succ": False}, 200
 
+    def __del__(self):
+        pass
+
+    def post(self):
+        try:
+            text = request.form["text"]
+            print(text)
+            return {"succ": True, "text": get_chatting(text)}, 200
+        except Exception as e:
+            print(e)
+            return {"text": "error while qa", "succ": False}, 200
